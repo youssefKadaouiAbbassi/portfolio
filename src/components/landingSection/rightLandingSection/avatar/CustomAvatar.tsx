7778f3e00f02824444158e14139bfd676f62a583
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2022-08-14 22:44:49 +0200

    Changing the Image component

--- src/components/landingSection/rightLandingSection/avatar/CustomAvatar.tsx
+++ src/components/landingSection/rightLandingSection/avatar/CustomAvatar.tsx
@@ -1,20 +1,20 @@
-import { Avatar, useMantineTheme } from '@mantine/core';
 import React from 'react';
+import Image from 'next/image';
 import { useMediaQuery, useViewportSize } from '@mantine/hooks';
 
 export const CustomAvatar: React.FC = () => {
   const isMobile = useMediaQuery('(max-width: 768px)');
   const { width } = useViewportSize();
-  const theme = useMantineTheme();
   return (
-    <Avatar
+    <Image
       src={
         isMobile
           ? '/assets/images/mobileAvatar.png'
           : '/assets/images/avatar.svg'
       }
-      size={width * (isMobile ? 600 / 768 : 600 / 1920)}
-      color={theme.colors.purple[0]}
+      width={width * (isMobile ? 600 / 768 : 600 / 1920)}
+      height={width * (isMobile ? 600 / 768 : 600 / 1920)}
+      priority
     />
   );
 };
