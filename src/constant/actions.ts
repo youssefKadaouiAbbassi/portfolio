a6109feabec1fe0a27a5ef6ceb94943cfb667ea1
Iznokill(Youssef Kadaoui Abbassi) <64014493+youssefKadaouiAbbassi@users.noreply.github.com>
2022-08-15 01:38:57 +0200

    Update actions.ts

--- src/constant/actions.ts
+++ src/constant/actions.ts
@@ -1,5 +1,5 @@
 export const actions = {
   github: 'https://github.com/youssefKadaouiAbbassi',
   linkedin: 'https://www.linkedin.com/in/youssef-kadaoui-abbassi/',
-  calendly: 'https://calendly.com/', // TODO: add calendly link
+  calendly: 'https://calendly.com/youssefkadaouiaabbassi/', 
 };
