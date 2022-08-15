e36662e3aa121e3af9493a84de9cb0535c7bc02f
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2022-08-15 02:12:39 +0200

    Better image using the center component

--- src/components/landingSection/rightLandingSection/avatar/CustomAvatar.tsx
+++ src/components/landingSection/rightLandingSection/avatar/CustomAvatar.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { useMediaQuery, useViewportSize } from '@mantine/hooks';
+import { Center, useMantineTheme } from '@mantine/core';
 import Image from 'next/image';
 
 export const CustomAvatar: React.FC = () => {
   const isMobile = useMediaQuery('(max-width: 768px)');
   const { width } = useViewportSize();
+  const theme = useMantineTheme();
+
   return (
-    <Image
-      src={'/assets/images/avatar.png'}
-      width={width * (isMobile ? 750 / 768 : 600 / 1920)}
-      height={width * (isMobile ? 750 / 768 : 600 / 1920)}
-      alt=""
-      priority
-    />
+    <Center
+      style={{
+        width: width * (isMobile ? 750 / 768 : 600 / 1920),
+        height: width * (isMobile ? 750 / 768 : 600 / 1920),
+        backgroundColor: theme.colors.blue[0],
+        borderRadius: '50%',
+      }}
+    >
+      <Image
+        src={'/assets/images/avatar.png'}
+        width={width * (isMobile ? 700 / 768 : 530 / 1920)}
+        height={width * (isMobile ? 700 / 768 : 530 / 1920)}
+        alt=""
+        priority
+      />
+    </Center>
   );
 };
